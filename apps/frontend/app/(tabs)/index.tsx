094ba1556b8933c4a48ce59257bf55f8a30712ff
Bikramjeet Sarmah <bikramjeet@boldtek.com>
2024-12-10 15:39:59 +0530

    Header: profile dropdown

--- apps/frontend/app/(tabs)/index.tsx
+++ apps/frontend/app/(tabs)/index.tsx
@@ -1,15 +1,11 @@
+import { ScrollView } from "react-native";
+
 import { Header } from "@/components/common/header";
-import useAuthStore from "@/hooks/useUser";
-import { View, Text, Button } from "react-native";
 
 export default function HomeScreen() {
-  const setAuth = useAuthStore((state) => state.setAuth);
-
   return (
-    <View>
+    <ScrollView>
       <Header />
-
-      <Button title="Logout" onPress={() => setAuth(false)} />
-    </View>
+    </ScrollView>
   );
 }
--- apps/frontend/components/common/header.tsx
+++ apps/frontend/components/common/header.tsx
@@ -1,9 +1,23 @@
-import { StatusBar, Text, View } from "react-native";
+import React from "react";
+import { View, Text, StatusBar, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export const Header = () => {
+import { ProfileDropdown } from "@/components/user/profile-dropdown";
+
+export function Header() {
   return (
-    <View style={{ paddingTop: (StatusBar.currentHeight || 0) + 10 }}>
-      <Text>Header</Text>
+    <View
+      className="flex-row items-center justify-between w-full p-4 mb-2 bg-white border-b border-gray-100 shadow-[0px_0px_10px]"
+      style={[{ paddingTop: (StatusBar.currentHeight || 0) + 10 }]}
+    >
+      <Ionicons name="menu" size={24} />
+
+      <View className="flex-row items-center gap-2">
+        <Image source={require("@/assets/images/logo.png")} />
+        <Text className="text-2xl font-semibold">QC</Text>
+      </View>
+
+      <ProfileDropdown />
     </View>
   );
-};
+}
